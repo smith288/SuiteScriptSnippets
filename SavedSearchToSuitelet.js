234e2b729b3959b59088a19d3b57265a2ab575a4
Erick Smith <Erick.Smith@pacejet.com>
2022-05-06 16:20:05 -0400

    Adding logging.

--- SavedSearchToSuitelet.js
+++ SavedSearchToSuitelet.js
@@ -1,31 +1,50 @@
 /*
-Run this snippet in the console of a browser in a Transaction record.  This will allow the require to work and perform the conversion to the Suitelet.
+This will allow the require to work and perform the conversion to the Suitelet.
+
+Directions: 
+Run this snippet in the console of a browser in a Transaction record or the Netsuite debugger using the 2.1 version.  
+
+Limitations:
+Saved search don't contain the values of which are saved within the saved search so I can only provide a placeholder that you must fill out yourself when you wish to utilize the search.
 */
-console.clear();
+if(!log){
+    log = console.log;
+    console.clear();
+}
+
 
 require(['N/record', 'N/search'], function(record, search) {
 
-    var srch = search.load({id: 'customsearch_mytest_search'});
-
-    var srch_filters_str = '', srch_columns_str = '';
-    srch.filters.forEach(x=> {
-        srch_filters_str += `\n\t\t\t{name:'${x.name}', operator:'${x.operator}'`;
-        if(x.join){
-            srch_filters_str += `, join: '${x.join}'`
-        }
-        srch_filters_str += `, values: [REPLACE]},`;
-    });
-    srch.columns.forEach(x=> {
-        srch_columns_str += `\n\t\t\t{name:'${x.name}'`;
-        if(x.join){
-            srch_columns_str += `, join: '${x.join}'`
-        }
-        srch_columns_str += `},`;
-    });
+    try {
+        
+        var srch = search.load({id: 'customsearch_mytest_search'});
     
-    srch_filters_str = srch_filters_str.substring(0,srch_filters_str.length-1);
-    srch_columns_str = srch_columns_str.substring(0,srch_columns_str.length-1);
-    var srch_str = `var srch = search.create(\n\t{type: '${srch.searchType}',\n\t\t filters: [${srch_filters_str}], \n\t\tcolumns: [${srch_columns_str}]\n});`;
-    console.log(srch_str);
+        var srch_filters_str = '', srch_columns_str = '';
+        srch.filters.forEach(x=> {
+            srch_filters_str += `\n\t\t\t{name:'${x.name}', operator:'${x.operator}'`;
+            if(x.join){
+                srch_filters_str += `, join: '${x.join}'`
+            }
+            srch_filters_str += `, values: [REPLACE]},`;
+        });
+        srch.columns.forEach(x=> {
+            srch_columns_str += `\n\t\t\t{name:'${x.name}'`;
+            if(x.join){
+                srch_columns_str += `, join: '${x.join}'`
+            }
+            srch_columns_str += `},`;
+        });
+        
+        srch_filters_str = srch_filters_str.substring(0,srch_filters_str.length-1);
+        srch_columns_str = srch_columns_str.substring(0,srch_columns_str.length-1);
+        var srch_str = `var srch = search.create(\n\t{type: '${srch.searchType}',\n\t\t filters: [${srch_filters_str}], \n\t\tcolumns: [${srch_columns_str}]\n});`;
+        
+        log.debug('SuiteScript', srch_str);
+    } catch (err) {
+        if(err.name == 'INVALID_SEARCH'){
+            log.error('That search does not exist');
+        }
+    }
+    
 
 });
