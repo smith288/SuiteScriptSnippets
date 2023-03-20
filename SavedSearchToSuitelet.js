901e68b9025981b2bd7732efe5c4ac8669b3ccdc
Erick <smith288@yahoo.com>
2023-03-19 22:18:44 -0400

    Added the ability to render the values found in the filter

--- SavedSearchToSuitelet.js
+++ SavedSearchToSuitelet.js
@@ -4,8 +4,7 @@ This will allow the require to work and perform the conversion to the Suitelet.
 Directions: 
 Run this snippet in the console of a browser in a Transaction record or the Netsuite debugger using the 2.1 version.  
 
-Limitations:
-Saved search don't contain the values of which are saved within the saved search so I can only provide a placeholder that you must fill out yourself when you wish to utilize the search.
+
 */
 if(!log){
     log = console.log;
@@ -18,14 +17,20 @@ require(['N/record', 'N/search'], function(record, search) {
     try {
         
         var srch = search.load({id: 'customsearch_mytest_search'});
-    
+
         var srch_filters_str = '', srch_columns_str = '';
         srch.filters.forEach(x=> {
+
             srch_filters_str += `\n\t\t\t{name:'${x.name}', operator:'${x.operator}'`;
             if(x.join){
                 srch_filters_str += `, join: '${x.join}'`
             }
-            srch_filters_str += `, values: [REPLACE]},`;
+            var values = `'${x.toJSON().values}'`;
+            if(x.toJSON().values.length > 1){
+                values = `[${x.toJSON().values}]`;
+            }
+            srch_filters_str += `, values: ${values}},`;
+
         });
         srch.columns.forEach(x=> {
             srch_columns_str += `\n\t\t\t{name:'${x.name}'`;
@@ -34,11 +39,11 @@ require(['N/record', 'N/search'], function(record, search) {
             }
             srch_columns_str += `},`;
         });
-        
+
         srch_filters_str = srch_filters_str.substring(0,srch_filters_str.length-1);
         srch_columns_str = srch_columns_str.substring(0,srch_columns_str.length-1);
         var srch_str = `var srch = search.create(\n\t{type: '${srch.searchType}',\n\t\t filters: [${srch_filters_str}], \n\t\tcolumns: [${srch_columns_str}]\n});`;
-        
+
         log.debug('SuiteScript', srch_str);
     } catch (err) {
         if(err.name == 'INVALID_SEARCH'){
